d082db144da933198852698f0076b98578800271
ChrisCTX <chrisctx@gmail.com>
2013-05-25 01:03:43 -0700

    Initial Commit
    GUI is pretty much done, drag and drop functionality is underway.
    Model and Database come later.

---
+++ bloGrid/bloGrid/Helpers/YoutubeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bloGrid.Helpers
+{
+    public static class YoutubeHelper
+    {
+        public static MvcHtmlString Youtube(this HtmlHelper helper, string url)
+        {
+            var tag = "<iframe class=\"story\" width=\"300\" height=\"200\" src=\"" + url + "\" frameborder=\"0\" allowfullscreen></iframe>";
+            return MvcHtmlString.Create(tag.ToString());
+        }
+    }
+}
